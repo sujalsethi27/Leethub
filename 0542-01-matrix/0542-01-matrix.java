33fe7bcac9d1b432c5bc9335b543fa9c44991b52
Sujal Sethi <sujalsethi468@gmail.com>
2026-09-11 00:28:55 +0530

    Time: 18 ms (12.96%), Space: 49.2 MB (44.63%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int[][] dist = new int[mat.length][mat[0].length];
+        int n = mat.length;
+        int m = mat[0].length; 
+    Queue<Pair> q = new LinkedList<>();
+    boolean[][] vis = new boolean[n][m]; 
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            if(mat[i][j] == 0 && !vis[i][j]) {
+                 vis[i][j] = true;
+                dist[i][j] = 0;
+            q.add(new Pair(i, j));
+            }
+       }
+     }  
+      while(!q.isEmpty()) {
+        Pair p = q.poll();
+        int row = p.row;
+        int col = p.col;
+        int[] dr = {-1, 1, 0, 0};
+        int[] dc = {0, 0, -1, 1};  
+        for(int k = 0; k < 4; k++) {
+        int newrow = row + dr[k];
+        int newcol = col + dc[k];   
+        if(newrow >= 0 && newrow < n && newcol >= 0 && newcol < m && mat[newrow][newcol] != 0 && !vis[newrow][newcol]) {
+     vis[newrow][newcol] = true;
+    dist[newrow][newcol] = dist[row][col] + 1;
+    q.add(new Pair(newrow, newcol));
+        } 
+        }
+         }
+     return dist;    
+   }
+ }
+
+class Pair {
+    int row;
+    int col;
+
+    Pair(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+}
